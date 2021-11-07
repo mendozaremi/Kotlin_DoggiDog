debf8a8996d555b68a75f6cf83e3aa332d4a9013
mendozaremi <mendozaremi@gmail.com>
2021-11-07 13:08:18 -0500

    completed dogimage kotlin class

--- app/src/main/java/com/remi/doggiedog/network/DogImageApiService.kt
+++ app/src/main/java/com/remi/doggiedog/network/DogImageApiService.kt
@@ -2,7 +2,7 @@ package com.remi.doggiedog.network
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLogginInterceptor
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -12,19 +12,29 @@ private const val BASE_URL = "https://dog.ceo/api/breeds/image/"
 
 // Added print out network requests and repsonse in logcat
 // Available via the dependency added in build.gradle
-private val networkLogginInterceptor =
-    HttpLoggingInterceptor().setLevel(HttpLoggingIntercerptor.Level.Body)
+private val networkLoggingInterceptor =
+    HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
 
+// Moshi will break down response to Json for usable data
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
+// Retrofit client print ok status code add json data moshi
 private val retrofit = Retrofit.Builder()
-    .client(OkHttpClient.Builder().addInterceptor(networkLogginInterceptor).build())
+    .client(OkHttpClient.Builder().addInterceptor(networkLoggingInterceptor).build())
     .addConverterFactory(MoshiConverterFactory.create(moshi))
-    .baseurl(BASE_URL)
+    .baseUrl(BASE_URL)
     .build()
 
+// Interface store
 interface DogImageApiService {
-    @GET()
+    // add "random" to the BASE_URL return one Dog image
+    @GET("random")
+    suspend fun getRandomDogImage(): DogImage
+}
+
+//
+object DogImageApi {
+    val retrofitService: DogImageApiService by lazy { retrofit.create(DogImageApiService::class.java)}
 }

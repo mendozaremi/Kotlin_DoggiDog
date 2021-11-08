7a133ba326e907cf9ba5a15fc0eab1c3a1d29175
mendozaremi <mendozaremi@gmail.com>
2021-11-08 17:30:29 -0500

    added color switching feature

--- app/src/main/java/com/remi/doggiedog/MainActivity.kt
+++ app/src/main/java/com/remi/doggiedog/MainActivity.kt
@@ -5,17 +5,27 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
+
+    // ColorWheel color Class
+    private val colorWheel = ColorWheel()
+
+    // Declare View Variable
+    private var constraintLayout: ConstraintLayout? = null
 
     // creates main layout with button and img resource
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val changeDogButton: Button = findViewById(R.id.button)
+        // assign view from layout files to the corresponding variables
+        constraintLayout = findViewById(R.id.constraintLayout)
 
         viewModel.currentlyDisplayedImage.observe( this,
             {
@@ -26,7 +36,16 @@ class MainActivity : AppCompatActivity() {
 
 
         changeDogButton.setOnClickListener {
+            val color = colorWheel.getColor()
+            val color2 = colorWheel.getColor()
+            // call getNewDog Function
             viewModel.getNewDog()
+
+            // change button color
+            changeDogButton.setBackgroundColor(color)
+
+            // change background color
+            constraintLayout!!.setBackgroundColor(color2)
         }
     }
 }
